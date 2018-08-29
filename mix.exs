8b773dca1fa3fb3ed94c2c79e54390f81a7a7ca3
Darin Wilson <darinwilson@gmail.com>
2018-08-29 15:25:06 -0700

    Allow Elixir 1.5 or greater

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MusicDB.MixProject do
     [
       app: :music_db,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
