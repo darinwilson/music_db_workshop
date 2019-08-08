57461841a806b2235a106ea4c6989b18f61f472d
Darin Wilson <darinwilson@gmail.com>
2019-08-07 20:08:19 -0700

    :arrow_up: Upgrade project for Ecto 3.x

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,6 @@ use Mix.Config
 whoami = String.replace(whoami, "\n", "")
 
 config :music_db, MusicDB.Repo,
-  adapter: Ecto.Adapters.Postgres,
   database: "music_db_workshop_dev",
   username: System.get_env("DATABASE_USERNAME") || whoami,
   password: System.get_env("DATABASE_PASSWORD") || nil,
